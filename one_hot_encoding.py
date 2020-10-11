2dce107c53f209f71a961ad91506c7092f40df5f
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-10-11 22:37:58 +0530

    Add files via upload

---
+++ one_hot_encoding.py
@@ -0,0 +1,7 @@
+# define the encoder function
+def one_hot_encode(labels):
+    n_labels = len(labels)
+    n_unique_labels = len(np.unique(labels))
+    one_hot_encode = np.zeros((n_labels, n_unique_labels))
+    one_hot_encode[np.arange(n_labels), labels] =1
+    return one_hot_encode
